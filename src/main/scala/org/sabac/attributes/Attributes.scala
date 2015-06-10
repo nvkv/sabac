8d315611aedb4c550bcf5e6117269601152b8e64
Semyon Novikov <me@sdfgh153.ru>
2015-06-10 16:20:31 +0500

    Fixed dumb issue with attributes merge

--- src/main/scala/org/sabac/attributes/Attributes.scala
+++ src/main/scala/org/sabac/attributes/Attributes.scala
@@ -2,18 +2,28 @@ package org.sabac.attributes
 
 class Attributes(as: (Any, Any)*) {
 
-  val attrs = as.groupBy(_._1).map { case (k, v) => (k, v.map(_._2).last) }
+  val attrs = as.groupBy(_._1).map { case (k, v) ⇒ (k, v.map(_._2).last) }
 
   def get(key: Any): Option[Any] = {
-    attrs.get(key)  
+    attrs.get(key)
   }
-  
+
   def merge(other: Attributes): Attributes = {
-    val mergedMap = ((this.attrs.keySet ++ other.attrs.keySet) map { i => 
-      i -> (this.attrs.get(i).toList ::: other.attrs.get(i).toList) 
+
+    def flatten(ls: List[Any]): List[Any] = ls flatMap {
+      case i: List[_] ⇒ flatten(i)
+      case e          ⇒ List(e)
+    }
+
+    val mergedMap = ((this.attrs.keySet ++ other.attrs.keySet) map { i ⇒
+      i -> (this.attrs.get(i).toList ::: other.attrs.get(i).toList)
     })
-    val result = mergedMap.toMap.map { case (k, values) => 
-      if (values.length == 1) (k, values.head) else (k, values) 
+    val result = mergedMap.toMap.map {
+      case (k, values) ⇒
+        if (values.length == 1)
+          (k, values.head)
+        else
+          (k, flatten(values))
     }
     new Attributes(result.toSeq: _*)
   }
